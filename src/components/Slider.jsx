db51efe10d4cfd046f4652bdb327ae3837a381f7
Yevhenii Nykytenko <y.nykytenko@smissltd.com>
2020-02-04 18:26:12 +0200

    slider finishing

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,64 +1,67 @@
 import React, { useState } from "react";
-import Counter from "./Counter";
 
 const HEIGHT = 500;
 const HANDLE_HALF_HEIGHT = 34;
 
 const Slider = () => {
-  const myRef = React.createRef();
+  const sliderRef = React.createRef();
   const [dragging, setDragging] = useState(false);
-  const [cordY, setCordY] = useState(0);
+  const [topOffset, setTopOffset] = useState(0);
 
   const _onMouseDown = event => {
     event.preventDefault();
     setDragging(true);
-    console.log("MOUSE DOWN", cordY);
+    console.log("MOUSE DOWN", topOffset);
   };
 
   const _onMouseUp = () => {
     setDragging(false);
-    console.log("MOUSE UP", cordY);
+    console.log("MOUSE UP", topOffset);
   };
 
   const _onMouseMove = event => {
-    console.log("MOUSE MOVE", cordY);
+    console.log("MOUSE MOVE", topOffset);
     if (!dragging) return;
 
-    const offset = event.clientY - myRef.current.getBoundingClientRect().top;
+    const offset =
+      event.clientY - sliderRef.current.getBoundingClientRect().top;
     if (offset >= 0 && offset <= HEIGHT) {
-      setCordY(offset);
+      setTopOffset(offset);
     } else if (offset < 0) {
-      setCordY(0);
+      setTopOffset(0);
     } else if (offset > HEIGHT) {
-      setCordY(HEIGHT);
+      setTopOffset(HEIGHT);
     }
   };
 
-  // const _onMouseOut = () => {
-  //   setDragging(false);
-  // };
+  const currentIndicatorNumber = Math.abs(topOffset / (HEIGHT / 100) - 100).toFixed(2);
+  const handlePosition = topOffset - HANDLE_HALF_HEIGHT;
+  const barBottomHeight = HEIGHT - topOffset;
 
   return (
-    <div
-      className="slider-container"
-      //onMouseOut={_onMouseOut}
-      onMouseMove={_onMouseMove}><span className='slider-indicator'>100</span>
-      <div className="slider-bar-top" ref={myRef} style={{ height: `${cordY}px` }}>
-        <div
-          className="slider-handle"
-          style={{ top: `${cordY - HANDLE_HALF_HEIGHT}px` }}
-          onMouseDown={_onMouseDown}
-          onMouseUp={_onMouseUp}>
-          <div className="arrow" />
-          <div className="handle">
-            <span>{Math.abs(cordY / (HEIGHT / 100) - 100).toFixed(2)}</span>
+    <div className="slider-container" onMouseMove={_onMouseMove}>
+      <div className="indicators-container" style={{ height: `${HEIGHT}px` }}>
+        <span className="slider-indicator">100</span>
+        <span className="slider-indicator">0</span>
+      </div>
+      <div className="slider" ref={sliderRef}>
+        <div className="slider-bar-top" style={{ height: `${topOffset}px` }}>
+          <div
+            className="slider-handle"
+            style={{ top: `${handlePosition}px` }}
+            onMouseDown={_onMouseDown}
+            onMouseUp={_onMouseUp}>
+            <div className="arrow" />
+            <div className="handle">
+              <span>{currentIndicatorNumber}</span>
+            </div>
           </div>
         </div>
+        <div
+          className="slider-bar-bottom"
+          style={{ height: `${barBottomHeight}px` }}
+        />
       </div>
-      <div
-        className="slider-bar-bottom"
-        style={{ height: `${HEIGHT - cordY}px` }}
-      />
     </div>
   );
 };
