73a48c9615004813128479abfe6847ba391d4a9d
Illia Trishchenko <i.trishchenko@smissltd.com>
2020-02-06 13:54:45 +0200

    fix table

--- src/shared/MainItemContainer.jsx
+++ src/shared/MainItemContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MainItemContainer = ({ name, children }) => {
+const MainItemContainer = ({ name = "", children }) => {
   return <div className={`main_item_container ${name}`}>{children}</div>;
 };
 
